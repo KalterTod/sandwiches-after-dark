2beb4f6d318cedc08d9404c7b61ea67e1c3b0748
Stephen Clark <steve.clark1989@gmail.com>
2021-12-08 18:55:43 -0500

    Add ability to add menu items

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-  	"fmt"
-  	"github.com/gorilla/mux"
-  	"log"
-  	"net/http"
-  	"sandwiches-after-dark/menu"
+	"fmt"
+	"log"
+	"net/http"
+	"sandwiches-after-dark/menu"
+
+	"github.com/gorilla/mux"
 )
 
 func handleRequests() {
-  	router := mux.NewRouter().StrictSlash(true)
-  	router.HandleFunc("/menu", menu.Menu).Methods("GET")
-  	router.HandleFunc("/item/{id}", menu.Item).Methods("GET")
-  	log.Fatal(http.ListenAndServe(":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/menu", menu.Menu).Methods("GET")
+	router.HandleFunc("/item/{id}", menu.Item).Methods("GET")
+	router.HandleFunc("/item", menu.PostItem).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
-  	fmt.Println("Sandwiches After Dark API v1.0")
-    handleRequests()
+	fmt.Println("Sandwiches After Dark API v1.0")
+	handleRequests()
 }
--- menu/api.go
+++ menu/api.go
@@ -1,63 +1,97 @@
 package menu
 
 import (
-    "encoding/json"
-    "net/http"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/bson"
-    "context"
-    "sandwiches-after-dark/db"
-    "github.com/gorilla/mux"
-    "go.mongodb.org/mongo-driver/bson/primitive"
+	"context"
+	"encoding/json"
+	"net/http"
+	"sandwiches-after-dark/db"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var Client *mongo.Client
 var CTX context.Context
 
 func Menu(response http.ResponseWriter, request *http.Request) {
-    // Set up content-type and define collection
-    response.Header().Set("content-type", "application/json")
-    collection := db.Session.Database("sandwiches-after-dark").Collection("menu")
+	// Set up content-type and define collection
+	response.Header().Set("content-type", "application/json")
+	collection := db.Session.Database("sandwiches-after-dark").Collection("menu")
 
-    // Create cursor to be used for result-set and set up error handling
-    cursor, currErr := collection.Find(CTX, bson.M{})
+	// Create cursor to be used for result-set and set up error handling
+	cursor, currErr := collection.Find(CTX, bson.M{})
 
-    if currErr != nil {
-        response.WriteHeader(http.StatusInternalServerError)
-        response.Write([]byte(`{"message": "` + currErr.Error() + `"}`))
-    }
-    defer cursor.Close(CTX)
+	if currErr != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + currErr.Error() + `"}`))
+	}
+	defer cursor.Close(CTX)
 
-    var items []MenuItem
-    for cursor.Next(CTX) {
-        var menuItem MenuItem
-        cursor.Decode(&menuItem)
-        items = append(items, menuItem)
-    }
+	var items []MenuItem
+	for cursor.Next(CTX) {
+		var menuItem MenuItem
+		cursor.Decode(&menuItem)
+		items = append(items, menuItem)
+	}
 
-    if err := cursor.Err(); err != nil {
-        response.WriteHeader(http.StatusInternalServerError)
-        response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-        return
-    }
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
 
-    json.NewEncoder(response).Encode(items)
+	json.NewEncoder(response).Encode(items)
 }
 
 func Item(response http.ResponseWriter, request *http.Request) {
-  response.Header().Set("content-type", "application/json")
-  params := mux.Vars(request)
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
 	id, _ := primitive.ObjectIDFromHex(params["id"])
-  collection := db.Session.Database("sandwiches-after-dark").Collection("menu")
+	collection := db.Session.Database("sandwiches-after-dark").Collection("menu")
 
-  var menuItem MenuItem
-  err := collection.FindOne(CTX, MenuItem{ID: id}).Decode(&menuItem)
+	var menuItem MenuItem
+	err := collection.FindOne(CTX, MenuItem{ID: id}).Decode(&menuItem)
 
-  if err != nil {
-      response.WriteHeader(http.StatusInternalServerError)
-      response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
-      return
-  }
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
 
-  json.NewEncoder(response).Encode(menuItem)
+	json.NewEncoder(response).Encode(menuItem)
 }
+
+func PostItem(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var newItem MenuItem
+
+	err := json.NewDecoder(request.Body).Decode(&newItem)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+
+	collection := db.Session.Database("sandwiches-after-dark").Collection("menu")
+
+	result, insertErr := collection.InsertOne(CTX, newItem)
+	if insertErr != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+
+	json.NewEncoder(response).Encode(result)
+}
+
+// func EditItem(response http.ResponseWriter, request *http.Request) {
+// 	response.Header().Set("content-type", "application/json")
+// 	params := mux.Vars(request)
+// 	id, _ := primitive.ObjectIDFromHex(params["id"])
+//
+// 	collection := db.Session.Database("sandwiches-after-dark").Collection("menu")
+// 	// result, updateErr := collection.UpdateByID(CTX, id, updateObject)
+//
+// }
